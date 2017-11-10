92ae39148bc4eee71d7200b4000f89b296f85b91
Jeanderson Candido <jeandersonbc@gmail.com>
2017-11-10 14:17:16 -0300

    Implemented BFS for shortest paths on unweighted graphs

--- src/main/java/com/example/graphsplayground/App.java
+++ src/main/java/com/example/graphsplayground/App.java
@@ -3,7 +3,18 @@ package com.example.graphsplayground;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
 import java.util.Scanner;
+import java.util.Set;
+
+import com.example.graphsplayground.model.AdjacencyList;
+import com.example.graphsplayground.model.Edge;
+import com.example.graphsplayground.model.Graph;
 
 public class App {
 
@@ -28,4 +39,45 @@ public class App {
 		return graph;
 	}
 
+	public static <T> Map<T, T> bfs(T source, T destiny, Graph<T> graph) {
+		Queue<T> queue = new LinkedList<>();
+		Set<T> visited = new HashSet<>();
+		Map<T, T> discoveredBy = new HashMap<>();
+
+		queue.add(source);
+		visited.add(source);
+		while (!queue.isEmpty()) {
+			T current = queue.remove();
+			if (current.equals(destiny)) {
+				break;
+			}
+			for (Edge<T> edge : graph.neighborsFrom(current)) {
+				T neighbor = edge.getVertexB();
+				if (!visited.contains(neighbor)) {
+					queue.add(neighbor);
+					visited.add(neighbor);
+					discoveredBy.put(neighbor, current);
+				}
+			}
+		}
+		return discoveredBy;
+	}
+
+	public static void main(String[] args) throws FileNotFoundException {
+		Graph<String> graph = App.loadGraph("data/digraph1.txt");
+		System.out.println(traverseFrom("A", "D", bfs("A", "D", graph)));
+	}
+
+	private static <T> List<T> traverseFrom(T source, T destiny, Map<T, T> path) {
+		LinkedList<T> traversal = new LinkedList<>();
+		T current = destiny;
+		traversal.addFirst(current);
+		while (!current.equals(source)) {
+			current = path.get(current);
+			if (current == null)
+				throw new RuntimeException("Unreachable path");
+			traversal.addFirst(current);
+		}
+		return traversal;
+	}
 }
